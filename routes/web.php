4d5fb304345d45a4458bf689d9f6e44419cf50ab
Tony Messias <tonysm@hey.com>
2024-01-08 10:15:25 -0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,24 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/up', function () {
+    return response(
+        <<<HTML
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Application is Up!</title>
+        </head>
+        <body style="height: 100%; background-color: green;">
+        </body>
+        </html>
+        HTML,
+        status: 200,
+    );
+});
+
 require __DIR__.'/auth.php';
